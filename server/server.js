3716d7381e696ade371c97137cc44652a793495c
Adam Samaha <adamsamahaa@gmail.com>
2016-04-13 14:22:17 -0700

    fix merge conflicts

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var bodyParser=require('body-parser');
 var app = express();
-var router=require('./server/router');
+// var router=require('./server/router');
 
 //set port variable to deployed port or 3000 for local host
 var port=process.env.PORT || 3000;
 
 //set dirname to client folder to serve static assets (index.html)s
-app.use('/',express.static(__dirname + '/client'));
+app.use('/',express.static(__dirname + '/../client'));
 
 //parses all incoming data from strings to JSON
 app.use(bodyParser.json());
 
 //Send all calls to router function
-app.use(router);
+// app.use(router);
 
 app.listen(port, function() {
   console.log("Listening on port " + port + "...");
